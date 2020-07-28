8df1d75deee0e6bd47145158d2bc6f3db9a2794c
deads3c <55228782+de4ds3c@users.noreply.github.com>
2020-07-28 20:16:02 +0300

    Update ip-geolocation-api-php.php

--- ip-geolocation-api-php.php
+++ ip-geolocation-api-php.php
@@ -15,6 +15,7 @@
         curl_setopt($cURL, CURLOPT_URL, $url);
         curl_setopt($cURL, CURLOPT_HTTPGET, true);
         curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURL, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Accept: application/json'
